cd645584981ce138e339c894cc04ba3b72dbd35e
Juan Tinizhañay Quito <juan.tinizhanay@outlok.com>
2018-09-04 09:06:15 -0500

    Maquetación y modificación de nombre de contenido.php por inicio.php
    Se procede a modificar la maquetacion de contenido.php y luego se renombra con inicio.php este mismo nombre se escribe en plantilla.php para incluir este módulo.

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -3,7 +3,9 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
   <title>POS</title>
+
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   
@@ -59,7 +61,7 @@
     CONTENIDO
     =============================================*/
 
-    include "modulos/contenido.php";
+    include "modulos/inicio.php";
 
     /*=============================================
     FOOTER
